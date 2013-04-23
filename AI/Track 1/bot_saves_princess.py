0840b053ebc153696ca2f56c9222b204226b7299
Hardeep Singh <ootz0rz@gmail.com>
2013-04-23 00:02:32 -0400

    yay rescue

--- AI/Track 1/bot_saves_princess.py
+++ AI/Track 1/bot_saves_princess.py
@@ -1,11 +1,14 @@
 '''
 https://www.hackerrank.com/challenges/saveprincess
 '''
+from math import sqrt
 
+# for input grid
 PRINCESS = 'p'
 YOU = 'm'
 EMPTY = '-'
 
+# valid moves/outputs
 UP = "UP"
 DOWN = "DOWN"
 LEFT = "LEFT"
@@ -20,7 +23,7 @@ def _parse_grid_input(grid):
 
 		Then the position of the princess as a tuple (x, y)
 	'''
-	
+
 	res = []
 	you_pos = []
 	prin_pos = []
@@ -43,21 +46,80 @@ def _parse_grid_input(grid):
 
 	return (res, you_pos, prin_pos)
 
-def _distance(px, py):
-	from math import sqrt
+def _distance((x1, y1), (x2, y2)):
+	'''
+	Euclidean distance between px, py, each given as tuple (x, y)
+	'''
+	
 	return sqrt(
-		(px[1] - px[0]) ** 2
+		(x2 - x1) ** 2
 		+
-		(py[1] - py[0]) ** 2
+		(y2 - y1) ** 2
 	)
 
+def _get_new_pos(grid, direction, cur):
+	'''
+	Given the grid, direction to move (UP DOWN LEFT RIGHT) and your current
+	position as a tuple (x, y), compute new position taking grid bounds into 
+	consideration.
+	'''
+
+	if direction == RIGHT:
+		if (cur[0] + 1) < len(grid):
+			cur = [cur[0] + 1, cur[1]]
+
+	elif direction == LEFT:
+		if (cur[0] - 1) >= 0:
+			cur = [cur[0] - 1, cur[1]]
+
+	elif direction == UP:
+		if (cur[1] - 1) >= 0:
+			cur = [cur[0], cur[1] - 1]
+
+	elif direction == DOWN:
+		if (cur[1] + 1) < len(grid[cur[0]]):
+			cur = [cur[0], cur[1] + 1]
+
+	return cur
+
 def displayPathtoPrincess(n, grid):
-	grid = _parse_grid_input(grid)
+	grid, me, target = _parse_grid_input(grid)
 
+	reached_target = False
 
+	cur_pos = [me[0], me[1]]
 
-	pass
+	move_list = (UP, DOWN, LEFT, RIGHT)
 
+	moves_used = []
+
+	while not (
+			# reached target?
+			cur_pos[0] == target[0] and cur_pos[1] == target[1]
+		):
+
+		# get possible moves
+		newpos = (
+			_get_new_pos(grid, move_list[0], cur_pos), 
+			_get_new_pos(grid, move_list[1], cur_pos), 
+			_get_new_pos(grid, move_list[2], cur_pos), 
+			_get_new_pos(grid, move_list[3], cur_pos))
+
+		# get new distances to target
+		tup = (
+			_distance(target, newpos[0]), 
+			_distance(target, newpos[1]), 
+			_distance(target, newpos[2]),
+			_distance(target, newpos[3]))
+
+		# find min
+		min_index = min(xrange(len(tup)), key=tup.__getitem__)
+
+		# update position+move
+		cur_pos = newpos[min_index]
+		moves_used.append(move_list[min_index])
+
+	return "\n".join(moves_used)
 
 import unittest
 
@@ -85,9 +147,31 @@ class DistanceCalc(unittest.TestCase):
 		px = (0,0)
 		py = (0,2)
 
-		res = 2.0
+		res = sqrt(2**2)
 
 		final = _distance(px, py)
+
+		self.assertEqual(res, final,
+			"Sample Input #00. Got %s expected %s." % (final, res))
+
+	def testSample01(self):
+		px = (0,2)
+		py = (1,1)
+
+		res = sqrt((2 - 1)**2 + (0 - 1)**2)
+
+		final = _distance(px, py)
+
+		self.assertEqual(res, final,
+			"Sample Input #01. Got %s expected %s." % (final, res))
+
+class PathDisplay(unittest.TestCase):
+
+	def testSample00(self):
+		grid = "---\n-%s-\n%s--" % (YOU, PRINCESS)
+		res = "DOWN\nLEFT"
+
+		final = displayPathtoPrincess(3, grid)
 
 		self.assertEqual(res, final,
 			"Sample Input #00. Got %s expected %s." % (final, res))
