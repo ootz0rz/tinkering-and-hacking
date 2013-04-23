8bf101847b04db6977c37fba83715ae1d3f04892
Hardeep Singh <ootz0rz@gmail.com>
2013-04-22 21:03:55 -0400

    sq1 paritioning

--- ALGORITHMS/Track 6/quick_sort_p1.py
+++ ALGORITHMS/Track 6/quick_sort_p1.py
@@ -1,6 +1,23 @@
-def partition(ar):
+'''
+https://www.hackerrank.com/challenges/quicksort1
 
-	return ""
+
+'''
+
+
+def partition(ar):
+	left = []
+	right = []
+	pivot = ar[0]
+
+	for i,c in enumerate(ar[1:]):
+		if c > pivot:
+			right.append(c)
+		else:
+			left.append(c)
+
+	print " ".join([str(x) for x in left + [pivot] + right])
+	return left + [pivot] + right
 
 import unittest
 
@@ -10,7 +27,7 @@ class SampleCases(unittest.TestCase):
 		ar = [int(x) for x in "4 5 3 7 2".split(" ")]
 		res = [int(x) for x in "3 2 4 5 7".split(" ")]
 
-		final = insertionSort(ar)
+		final = partition(ar)
 
 		self.assertEqual(res, final,
 			"Sample Input #00. Got %s expected %s." % (final, res))
