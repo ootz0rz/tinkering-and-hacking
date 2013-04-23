274a6acb39f691dc4ac6aeb77aa46797747b12b5
Hardeep Singh <ootz0rz@gmail.com>
2013-04-23 01:47:49 -0400

    some help from other sites...that -~x dealy. there must be more i can do though :o

--- NORMAL LANGUAGES/fizzbuzz.py
+++ NORMAL LANGUAGES/fizzbuzz.py
@@ -1,1 +1,4 @@
-for x in range(100):print x%3/2*"Fizz"+x%5/4*"Buzz"or x+1
+for x in range(100):print x%3/2*"Fizz"+x%5/4*"Buzz"or-~x
+#x=0;exec'print x%3/2*"Fizz"+x%5/4*"Buzz"or-~x;x+=1;'*100
+
+# https://sites.google.com/site/codegolfingtips/python
