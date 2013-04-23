1eeefbf49e0f98683870089f9e918adb46439c83
Hardeep Singh <ootz0rz@gmail.com>
2013-04-23 01:05:35 -0400

    single line

--- NORMAL LANGUAGES/fizzbuzz.py
+++ NORMAL LANGUAGES/fizzbuzz.py
@@ -1,11 +1,1 @@
-for x in range(16):
-	if x % 3 == 0:
-		print "Fizz",
-
-	if x % 5 == 0:
-		print "Buzz",
-
-	if x % 3 != 0 and x % 5 != 0:
-		print x,
-
-	print
+for x in range(1,16): print "Fizz" * (x % 3 == 0) + "Buzz" * (x % 5 == 0) or x
