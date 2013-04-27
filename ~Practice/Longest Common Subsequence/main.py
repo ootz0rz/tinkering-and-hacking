247e5ea4b43a8c3dec31b515f40cffd0db4aa4f8
Hardeep Singh <ootz0rz@gmail.com>
2013-04-27 18:12:51 -0400

    initial version coding to mirror the final formulation from the readme

---
+++ ~Practice/Longest Common Subsequence/main.py
@@ -0,0 +1,39 @@
+'''
+See readme.md for what this code is based off of.
+'''
+
+def LCS(X, Y, mem={}):
+	'''
+	Compute the Longest Common Subsequence of sequences X and Y
+	'''
+
+	i = len(X)
+	j = len(Y)
+
+	# init mem
+	if not (i in mem):
+		mem[i] = {}
+
+	if not (j in mem[i])
+		mem[i][j] = []
+	
+	# LCS of any set and an empty set is the empty set
+	if i == 0 or j == 0:
+		mem[i][j] = []
+
+	# Case 1: endings are same
+	elif X[-1] == Y[-1]:
+		mem[i][j] = \
+			LCS(X[:-1], Y[:-1], mem) + [X[-1]]
+
+	# Case 2: endings are not the same
+	elif X[-1] != Y[-1]:
+		mX = LCS(X[:-1], Y, mem)
+		mY = LCS(X, Y[:-1], mem)
+
+		if len(mX) > len(mY):
+			mem[i][j] = mX
+		else:
+			mem[i][j] = mY
+
+	return mem[i][j]
