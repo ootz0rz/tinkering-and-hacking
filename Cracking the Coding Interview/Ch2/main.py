a7f2d49e440367aa250d3a576d1388a0bb1e5d8f
Hardeep <ootz0rz@gmail.com>
2014-10-21 21:04:58 -0700

    Some more linked list related ones

---
+++ Cracking the Coding Interview/Ch2/main.py
@@ -0,0 +1,93 @@
+class Node(object):
+    '''
+    Helper class for questions below.
+    '''
+    n = None
+    data = None
+
+    def __init__(self, d=None):
+        self.data = d
+
+    def append(self, d):
+        end = Node(d)
+        n = self
+
+        while n.n is not None:
+            n = n.n
+
+        n.n = end
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        n = self
+        l = [n.data]
+
+        while n.n is not None:
+            n = n.n
+            l.append(n.data)
+
+        return str(l)
+
+def q2_2():
+    '''
+    Basically keep two cursors. One is used to move to the end, the second will
+    lag n spaces behind to give you the nth to last element within the list.
+    '''
+    pass
+
+def q2_3(n):
+    '''
+    Given a node somewhere within a linked list, delete it but keep the LL
+    continuous.
+    '''
+
+    # idea: copy next node to current node, delete next node
+    pass
+
+def q2_4(n1, n2):
+    '''
+    Given two numbers stored as linked lists in reverse order such that the
+    head contains the 1s digit, add the two numbers and return the sum as a
+    linked list.
+    '''
+    _n1 = n1
+    _n2 = n2
+
+    rollover = 0
+    out = Node()
+    cur_node = out
+    while True:
+        left = 0
+        right = 0
+
+        # get values to add
+        if n1 is not None:
+            left = n1.data
+        if n2 is not None:
+            right = n2.data
+
+        # add
+        total = left + right + rollover
+        cur_val = total % 10
+        rollover = 1 if total >= 10 else 0
+
+        # store in output
+        cur_node.data = cur_val
+
+        # update pointers
+        if n1 is not None: n1 = n1.n
+        if n2 is not None: n2 = n2.n
+
+        # if both n1 and n2 are None, and nothing to roll over, 
+        # then we've finished
+        if (n1 is None) and (n2 is None) and rollover == 0:
+            break
+
+        # otherwise, lets add another node and continue
+        cur_node.n = Node()
+        cur_node = cur_node.n
+
+    print _n1, '+', _n2, '=', out
+    return out
