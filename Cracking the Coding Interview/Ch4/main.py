23ed4ed0e6443b66ffbae93491aa7c7843aa7ab1
Hardeep <ootz0rz@gmail.com>
2014-10-22 20:34:23 -0700

    * Print function for Tree Nodes  * Couple of more Qs

--- Cracking the Coding Interview/Ch4/main.py
+++ Cracking the Coding Interview/Ch4/main.py
@@ -1,4 +1,5 @@
 class TNode(object):
+
     '''
     Helper class for binary tree nodes.
     '''
@@ -7,6 +8,35 @@ class TNode(object):
         self.data = data
         self.L = left
         self.R = right
+
+    def __eq__(self, o):
+        return \
+            self.data == o.data \
+            and self.L == o.L \
+            and self.R == o.R
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return "<TNode:%s,L=%s,R=%s>" % (self.data, self.L, self.R, )
+
+
+    def p(self, indent=0):
+        '''
+        Print a pretty representation of the tree rooted at this node.
+        '''
+        if self.R is not None:
+            self.R.p(indent + 1)
+        else:
+            print '\t' * (indent + 1), "None"
+            
+        print '\t' * indent, self.data
+
+        if self.L is not None:
+            self.L.p(indent + 1)
+        else:
+            print '\t' * (indent + 1), "None"
 
 
 def q4_1(root):
@@ -30,3 +60,36 @@ def q4_1(root):
 
     # check difference between the two
     return (_max - _min) <= 1
+
+
+def q4_2(graph, n1, n2):
+    '''
+    Given a directed graph, return True iff there is a path between n1 and n2.
+    '''
+
+    # Idea: start at n1, do a brute force search for n2
+
+    # Could also use something like dijkstra's or A* or w/e for perf later
+
+
+def q4_3(arr):
+    '''
+    Create a balanced binary tree with from the values in the given array, arr.
+
+    Assumes arr is sorted.
+    '''
+    if arr is None or len(arr) == 0:
+        return None
+
+    # Take the middle element as current node, then add all the items to the
+    # left recursively, and likewise to the right
+    mid = len(arr) / 2
+    mid_val = arr[mid]
+    left_arr = arr[:mid]
+    right_arr = arr[mid + 1:]
+
+    node = TNode(data=mid_val,
+                 left=q4_3(left_arr),
+                 right=q4_3(right_arr))
+
+    return node
--- Cracking the Coding Interview/Ch4/tests.py
+++ Cracking the Coding Interview/Ch4/tests.py
@@ -1,22 +1,37 @@
 from main import *
 
-class test_q4_1(object):
-    EMPTY = None
-    SINGLE_NODE = TNode(0)
-    SIMPLE_FULL = TNode(0, left=TNode(1), right=TNode(2))
-    SIMPLE_UNBALANCED_LEFT_LINE = TNode(0, left=TNode(1, left=TNode(2)))
+EMPTY = None
+SINGLE_NODE = TNode(0)
+SIMPLE_FULL = TNode(1, left=TNode(0), right=TNode(2))
+SIMPLE_UNEVEN = TNode(2, left=TNode(1, left=TNode(0)), right=TNode(3))
+SIMPLE_UNBALANCED_LEFT_LINE = TNode(0, left=TNode(1, left=TNode(2)))
 
+class test_q4_1(object):
     def test_empty(self):
-        assert q4_1(self.EMPTY) == True, "Empty tree"
+        assert q4_1(EMPTY) == True, "Empty tree"
 
     def test_single_node(self):
-        assert q4_1(self.SINGLE_NODE) == True, "Single Node"
+        assert q4_1(SINGLE_NODE) == True, "Single Node"
 
     def test_simple_full_tree(self):
-        assert q4_1(self.SIMPLE_FULL) == True, "Simple full tree"
+        assert q4_1(SIMPLE_FULL) == True, "Simple full tree"
 
     def test_simple_unbalanced(self):
-        assert q4_1(self.SIMPLE_UNBALANCED_LEFT_LINE) == False, "Simple unbalanced tree"
+        assert q4_1(SIMPLE_UNBALANCED_LEFT_LINE) == False, "Simple unbalanced tree"
+
+class test_q4_3(object):
+
+    def test_empty(self):
+        assert q4_3([]) == EMPTY, "Empty list"
+
+    def test_single_node(self):
+        assert q4_3([0]) == SINGLE_NODE, "Single Node"
+
+    def test_simple_full_tree(self):
+        assert q4_3([0, 1, 2]) == SIMPLE_FULL, "Simple full tree"
+
+    def test_simple_uneven_tree(self):
+        assert q4_3([0, 1, 2, 3]) == SIMPLE_UNEVEN, "Simple uneven tree"
 
 if __name__ == '__main__':
     import nose
