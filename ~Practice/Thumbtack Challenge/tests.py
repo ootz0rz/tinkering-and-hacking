c5de414fb08398f7ea68f3a7e5143e946c4bf0b2
Hardeep <ootz0rz@gmail.com>
2014-10-27 19:51:37 -0700

    more tests

--- ~Practice/Thumbtack Challenge/tests.py
+++ ~Practice/Thumbtack Challenge/tests.py
@@ -5,14 +5,35 @@ class TestSimpleDB(object):
     def setup(self):
         self.d = SimpleDB()
 
+    def test_get_non_existing_value(self):
+        assert self.d.parse_and_exec('GET ex') == sNULL
+        assert self.d.parse_and_exec('END') is None
+
     def test_set_and_get_single_value(self):
-        '''
-        SET ex 10
-        GET ex
-        END
-        '''
         assert self.d.parse_and_exec('SET ex 10') is None
         assert self.d.parse_and_exec('GET ex') == 10
+        assert self.d.parse_and_exec('END') is None
+
+    def test_set_value_twice(self):
+        assert self.d.parse_and_exec('GET ex') == sNULL
+
+        assert self.d.parse_and_exec('SET ex 10') is None
+        assert self.d.parse_and_exec('GET ex') == 10
+
+        assert self.d.parse_and_exec('SET ex 20') is None
+        assert self.d.parse_and_exec('GET ex') == 20
+
+    def test_set_and_get_multiple_values(self):
+        assert self.d.parse_and_exec('GET ex') == sNULL
+
+        assert self.d.parse_and_exec('SET ex 10') is None
+        assert self.d.parse_and_exec('GET ex') == 10
+
+        assert self.d.parse_and_exec('SET ex2 20') is None
+        assert self.d.parse_and_exec('GET ex2') == 20
+
+        assert self.d.parse_and_exec('GET ex') == 10
+
         assert self.d.parse_and_exec('END') is None
 
 if __name__ == '__main__':
