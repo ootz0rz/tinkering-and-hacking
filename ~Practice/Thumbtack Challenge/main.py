51fd5962f1ad8280e697e0611acc0e54b7374fb7
Hardeep <ootz0rz@gmail.com>
2014-10-27 21:48:52 -0700

    transactions complete i think

--- ~Practice/Thumbtack Challenge/main.py
+++ ~Practice/Thumbtack Challenge/main.py
@@ -8,11 +8,34 @@ sNULL = 'NULL'
 sBEGIN = 'BEGIN'
 sROLLBACK = 'ROLLBACK'
 sCOMMIT = 'COMMIT'
+sNO_TRANSACTION = 'NO TRANSACTION'
 
 class SimpleDB(object):
     '''
     A simple database that allows for storage and retrieval of data, and basic
     transactional commits, designed to be used from a command line interface.
+
+    Some notes on design decisions:
+        I made the GET/SET/UNSET/NUMEQUALTO methods as fast as possible, all 
+        run in O(1) respective to number of total variables in the database. 
+        This is because there is only ever a single state for the database
+        stored as two dictionaries: one for the name/value pairs, and another 
+        for the counts of values. These are updated as operations are executed.
+        This means, to keep NUMEQUALTO fast, additional memory is required for
+        its corresponding dictionary as well.
+
+        Transactions currently store the reverse of any commands executed, so 
+        that they may be literally rolled back. This might end up requiring a 
+        lot of memory for larger transactions, as a lot of state information 
+        is stored. 
+
+        This means that commit operations are super fast as well, as nothing 
+        needs to be done except removing the transaction state information 
+        from memory.
+
+        Thus, the only slow operation is ROLLBACK which runs in O(M) where M 
+        is the number of commands executed within that transaction that modify
+        any values.
     '''
 
     def __init__(self):
@@ -35,19 +58,19 @@ class SimpleDB(object):
         # counts
         self.COUNTS = {}
 
-        # transactional stacks
-        self.TRANSACTION_STACKS = []
-        self.in_transaction = False
-        self.cur_transaction = -1
+        self.__reset_transactions()
 
     def is_in_transaction(self):
         return self.in_transaction
+
+    def get_transaction_level(self):
+        return len(self.TRANSACTION_STACKS)
 
     def parse_and_exec(self, line, print_debug=False):
         '''
         Parse given line from CLI, and execute equivalent method.
         '''
-        tokens = line.split(None, 1)
+        tokens = line.split()
         command = tokens[0]
         args = tokens[1:]
 
@@ -57,24 +80,22 @@ class SimpleDB(object):
         if command in [sSET, sGET, sUNSET]:
 
             # these commands specify a value after the name
-            if len(args) > 0:
-                args = args[0].split(None, 1)
-
-                if command in [sSET]:
+            if command in [sSET]:
+                if len(args) > 0:
                     args[1] = int(args[1], 10)
 
         # these commands have the value as first arg
         elif command in [sNUMEQUALTO]:
             args[0] = int(args[0], 10)
 
-        retVal = self._run_command(command, args)
+        retVal = self._run_cli_command(command, args)
 
         if print_debug:
             print line, '->', retVal
 
         return retVal
 
-    def _run_command(self, command, args):
+    def _run_cli_command(self, command, args):
         '''
         Given the CLI text for the command and the parse argument list, run
         the corresponding method.
@@ -83,10 +104,12 @@ class SimpleDB(object):
         return func(*args)
 
     def _set(self, name, value):
+        # retain old value for count update
         old_val = None
         if name in self.CUR_DATA:
             old_val = self.CUR_DATA[name]
 
+        # set new val
         self.CUR_DATA[name] = value
 
         # update counts
@@ -99,6 +122,13 @@ class SimpleDB(object):
                 del self.COUNTS[old_val]
             else:
                 self.COUNTS[old_val] -= 1
+
+        # update transaction state if necessary
+        if self.is_in_transaction():
+            if old_val is not None:
+                self.TRANSACTION_STACKS[-1].append((sSET, [name, old_val]))
+            else:
+                self.TRANSACTION_STACKS[-1].append((sUNSET, [name]))
 
     def _get(self, name):
         if not (name in self.CUR_DATA):
@@ -118,6 +148,10 @@ class SimpleDB(object):
             else:
                 del self.COUNTS[cur_val]
 
+            # update transaction state if necessary
+            if self.is_in_transaction():
+                self.TRANSACTION_STACKS[-1].append((sSET, [name, cur_val]))
+
     def _num_equal_to(self, value):
         if value in self.COUNTS:
             return self.COUNTS[value]
@@ -130,16 +164,41 @@ class SimpleDB(object):
     def _begin(self):
         if not self.is_in_transaction():
             self.in_transaction = True
-            self.cur_transaction = -1
 
-        self.cur_transaction += 1
+        self.TRANSACTION_STACKS.append([])
 
     def _commit(self):
+        if self.is_in_transaction():
+            self.__reset_transactions()
+        else:
+            return sNO_TRANSACTION
+
+    def _rollback(self):
+        if self.is_in_transaction():
+            # roll back the most recent transaction only
+            cur_stack = self.TRANSACTION_STACKS.pop()
+
+            self.in_transaction = False
+            while len(cur_stack) > 0:
+                command, args = cur_stack.pop()
+                self._run_cli_command(command, args)
+            self.in_transaction = True
+
+            if self.get_transaction_level() == 0:
+                self.__reset_transactions()
+        else:
+            return sNO_TRANSACTION
+
+    def __reset_transactions(self):
         self.in_transaction = False
         self.cur_transaction = -1
 
-    def _rollback(self):
-        pass
+        # transactional stacks, each will hold a list of all the commands 
+        # required to revert to a state before the transaction began
+        #
+        # Format:
+        #   [(cmd, [arg1, arg2, ...]), (cmd2, [arg1, ...]), ...]
+        self.TRANSACTION_STACKS = []
 
 if __name__ == '__main__':
     d = SimpleDB()
--- ~Practice/Thumbtack Challenge/tests.py
+++ ~Practice/Thumbtack Challenge/tests.py
@@ -175,62 +175,261 @@ class TestSimpleDB(object):
 
     def test_default_no_transaction(self):
         assert not self.d.is_in_transaction() 
-        assert self.d.cur_transaction == -1
+        assert self.d.get_transaction_level() == 0
+
+    def test_default_commit_null_transaction(self):
+        assert self.parse('COMMIT') == sNO_TRANSACTION
+
+    def test_default_rollback_null_transaction(self):
+        assert self.parse('ROLLBACK') == sNO_TRANSACTION
 
     def test_start_transaction(self):
         assert self.parse('BEGIN') is None
         assert self.d.is_in_transaction()
-        assert self.d.cur_transaction == 0
+        assert self.d.get_transaction_level() == 1
 
         assert self.parse('END') is None
 
     def test_simple_start_and_commit_empty_transaction(self):
         assert self.parse('BEGIN') is None
         assert self.d.is_in_transaction()
-        assert self.d.cur_transaction == 0
+        assert self.d.get_transaction_level() == 1
 
         assert self.parse('COMMIT') is None
         assert not self.d.is_in_transaction() 
-        assert self.d.cur_transaction == -1
+        assert self.d.get_transaction_level() == 0
+
+        assert self.parse('ROLLBACK') == sNO_TRANSACTION
+        assert self.parse('COMMIT') == sNO_TRANSACTION
 
         assert self.parse('END') is None
 
     def test_simple_start_and_commit_changes_transaction(self):
         assert self.parse('SET ex 10') is None
         assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
 
         assert self.parse('BEGIN') is None
         assert self.d.is_in_transaction()
-        assert self.d.cur_transaction == 0
+        assert self.d.get_transaction_level() == 1
 
         assert self.parse('SET ex 20') is None
         assert self.parse('GET ex') == 20
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 1
 
         assert self.parse('COMMIT') is None
         assert not self.d.is_in_transaction() 
-        assert self.d.cur_transaction == -1
+        assert self.d.get_transaction_level() == 0
         assert self.parse('GET ex') == 20
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 1
 
         assert self.parse('END') is None
 
-    def test_simple_start_and_rollback_changes_transaction(self):
+    def test_simple_start_and_rollback_set_changes_transaction(self):
         assert self.parse('SET ex 10') is None
         assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
 
         assert self.parse('BEGIN') is None
         assert self.d.is_in_transaction()
-        assert self.d.cur_transaction == 0
+        assert self.d.get_transaction_level() == 1
 
         assert self.parse('SET ex 20') is None
         assert self.parse('GET ex') == 20
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 1
 
         assert self.parse('ROLLBACK') is None
         assert not self.d.is_in_transaction() 
-        assert self.d.cur_transaction == -1
+        assert self.d.get_transaction_level() == 0
         assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
 
         assert self.parse('END') is None
 
+    def test_start_and_rollback_set_changes_transaction(self):
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 1
+
+        assert self.parse('SET ex 10') is None
+        assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 2
+
+        assert self.parse('SET ex 20') is None
+        assert self.parse('GET ex') == 20
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 1
+
+        assert self.parse('ROLLBACK') is None
+        assert self.d.is_in_transaction() 
+        assert self.d.get_transaction_level() == 1
+        assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('ROLLBACK') is None
+        assert not self.d.is_in_transaction() 
+        assert self.d.get_transaction_level() == 0
+        assert self.parse('GET ex') == sNULL
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('END') is None
+
+    def test_start_and_rollback_unset_changes_transaction(self):
+        assert self.parse('SET ex 10') is None
+        assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 1
+
+        assert self.parse('SET ex 20') is None
+        assert self.parse('GET ex') == 20
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 1
+
+        assert self.parse('UNSET ex') is None
+        assert self.parse('GET ex') == sNULL
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('ROLLBACK') is None
+        assert not self.d.is_in_transaction() 
+        assert self.d.get_transaction_level() == 0
+        assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('END') is None
+
+    def test_rollback_after_commit_transaction(self):
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 1
+
+        assert self.parse('SET ex 30') is None
+        assert self.parse('GET ex') == 30
+        assert self.parse('NUMEQUALTO 30') == 1
+        assert self.parse('NUMEQUALTO 40') == 0
+
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 2
+
+        assert self.parse('SET ex 40') is None
+        assert self.parse('GET ex') == 40
+        assert self.parse('NUMEQUALTO 30') == 0
+        assert self.parse('NUMEQUALTO 40') == 1
+
+        assert self.parse('COMMIT') is None
+        assert not self.d.is_in_transaction() 
+        assert self.d.get_transaction_level() == 0
+
+        assert self.parse('GET ex') == 40
+        assert self.parse('NUMEQUALTO 30') == 0
+        assert self.parse('NUMEQUALTO 40') == 1
+
+        assert self.parse('ROLLBACK') == sNO_TRANSACTION
+
+        assert self.parse('GET ex') == 40
+        assert self.parse('NUMEQUALTO 30') == 0
+        assert self.parse('NUMEQUALTO 40') == 1
+
+        assert self.parse('END') is None
+
+    def test_start_and_rollback_once_and_commit_changes_transaction(self):
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 1
+
+        assert self.parse('SET ex 10') is None
+        assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 2
+
+        assert self.parse('SET ex 20') is None
+        assert self.parse('GET ex') == 20
+        assert self.parse('NUMEQUALTO 10') == 0
+        assert self.parse('NUMEQUALTO 20') == 1
+
+        assert self.parse('ROLLBACK') is None
+        assert self.d.is_in_transaction() 
+        assert self.d.get_transaction_level() == 1
+        assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('COMMIT') is None
+        assert not self.d.is_in_transaction() 
+        assert self.d.get_transaction_level() == 0
+        assert self.parse('GET ex') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+        assert self.parse('NUMEQUALTO 20') == 0
+
+        assert self.parse('END') is None
+
+    def test_start_and_rollback_multiple_variables_transaction(self):
+        assert self.parse('NUMEQUALTO 10') == 0
+
+        assert self.parse('SET a 10') is None
+        assert self.parse('GET a') == 10
+        assert self.parse('NUMEQUALTO 10') == 1
+
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 1
+        assert self.parse('SET b 10') is None
+        assert self.parse('GET b') == 10
+        assert self.parse('NUMEQUALTO 10') == 2
+
+        assert self.parse('BEGIN') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 2
+        assert self.parse('SET c 10') is None
+        assert self.parse('GET c') == 10
+        assert self.parse('NUMEQUALTO 10') == 3
+
+        assert self.parse('ROLLBACK') is None
+        assert self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 1
+        assert self.parse('GET a') == 10
+        assert self.parse('GET b') == 10
+        assert self.parse('GET c') == sNULL
+        assert self.parse('NUMEQUALTO 10') == 2
+
+        assert self.parse('ROLLBACK') is None
+        assert not self.d.is_in_transaction()
+        assert self.d.get_transaction_level() == 0
+        assert self.parse('GET a') == 10
+        assert self.parse('GET b') == sNULL
+        assert self.parse('GET c') == sNULL
+        assert self.parse('NUMEQUALTO 10') == 1
+
+        assert self.parse('END') is None
 
 if __name__ == '__main__':
     import nose
