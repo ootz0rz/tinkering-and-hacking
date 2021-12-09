eca7e19374cac7c84bf6aa145456a39ca481e423
Hardeep Singh <ootz0rz@gmail.com>
2021-12-09 18:56:48 -0500

    cleanup... but probably should redo this one and make it cleaner
    consider a while loop... while left < right, left=min, right=max sweet per chunk

--- 2021/LeetCode/AlgoIII/1231 Divide Chocolate v2.py
+++ 2021/LeetCode/AlgoIII/1231 Divide Chocolate v2.py
@@ -42,10 +42,7 @@ class Solution:
         min_sweet = min(sweetness)
         max_sweet = sum(sweetness) // num_peeps
 
-        if debug: print(f'\n-----\nsweetness: {sweetness}, num_peeps: {num_peeps}, range[{min_sweet}, {max_sweet}]')
-
-        num_chunks, smallest = _maximizeSweetness(sweetness, num_peeps, min_sweet, max_sweet)
-        return smallest
+        return _maximizeSweetness(sweetness, num_peeps, min_sweet, max_sweet)[1]
 
 
 def _maximizeSweetness(sweetness: List[int], num_peeps: int, min_sweet: int, max_sweet: int) -> List[int]:
@@ -56,46 +53,7 @@ def _maximizeSweetness(sweetness: List[int], num_peeps: int, min_sweet: int, max
 
     sweetness_target = min_sweet + (max_sweet - min_sweet) // 2
 
-    if debug: print(f'\tnum_peeps: {num_peeps} range[{min_sweet}:{max_sweet}] target: {sweetness_target}')
-
-    num_chunks, smallest_chunk = _get_chunks(sweetness, sweetness_target)
-
-    if debug: print(f'\t\tchunks: {num_chunks}, smallest: {smallest_chunk}, target: {sweetness_target}')
-
-    if num_chunks < num_peeps:
-        if min_sweet == max_sweet:
-            if debug: print(f'\t\t --> left end of line: [num: {num_chunks}, smallest: {smallest_chunk}]')
-            return [num_chunks, smallest_chunk]
-
-        # we failed to get a large enough value
-        if debug: print(f'\t\t --> left range: [{min_sweet}:{sweetness_target-1}]')
-        left_num, left_val = _maximizeSweetness(sweetness, num_peeps, min_sweet, sweetness_target-1)
-
-        if left_num >= num_peeps:
-            if debug: print(f'\t\t --> left valid: [num: {left_num}, smallest: {left_val}]')
-            return [left_num, left_val]
-        else:
-            if debug: print(f'\t\t --> left invalid: [num: {num_chunks}, smallest: {smallest_chunk}]')
-            return [num_chunks, smallest_chunk]
-
-    if num_chunks >= num_peeps:
-        # we have at least as many chunks as peeps
-
-        # we need to check the right hand side and see if there's another better value
-        if debug: print(f'\t\t --> right range: [{sweetness_target+1}:{max_sweet}]')
-        right_num, right_val = _maximizeSweetness(sweetness, num_peeps, sweetness_target+1, max_sweet)
-
-        if right_num >= num_peeps:
-            if debug: print(f'\t\t --> right valid: [num: {right_num}, smallest: {right_val}]')
-            return [right_num, right_val]
-        else:
-            if debug: print(f'\t\t --> right invalid: [num: {num_chunks}, smallest: {smallest_chunk}]')
-            return [num_chunks, smallest_chunk]
-
-def _get_chunks(sweetness: List[int], sweetness_target: int) -> List[int]:
-    """
-    return [num_chunks, smallest_val]
-    """
+    # get chunks
     idx = 0
 
     num_chunks = 0
@@ -116,7 +74,30 @@ def _get_chunks(sweetness: List[int], sweetness_target: int) -> List[int]:
 
         idx = idx + 1
 
-    return [num_chunks, smallest_chunk]
+    # do we have enough chunks?
+    if num_chunks < num_peeps:
+        if min_sweet == max_sweet:
+            # we don't...but nothing left to search either
+            return [num_chunks, smallest_chunk]
+
+        # check left side to see if we can get things working
+        left = _maximizeSweetness(sweetness, num_peeps, min_sweet, sweetness_target-1)
+
+        if left[0] >= num_peeps:
+            return left
+        else:
+            return [num_chunks, smallest_chunk]
+
+    if num_chunks >= num_peeps:
+        # we have at least as many chunks as peeps
+
+        # we need to check the right hand side and see if there's another better value
+        right = _maximizeSweetness(sweetness, num_peeps, sweetness_target+1, max_sweet)
+
+        if right[0] >= num_peeps:
+            return right
+        else:
+            return [num_chunks, smallest_chunk]
 
 if __name__ == '__main__':
     debug = True
