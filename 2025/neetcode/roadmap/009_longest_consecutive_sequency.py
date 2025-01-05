4f2a12012b0ecad56377d8d3004e63c03f9f4c70
H S <ootz0rz@gmail.com>
2025-01-05 18:27:10 -0500

    .

---
+++ 2025/neetcode/roadmap/009_longest_consecutive_sequency.py
@@ -0,0 +1,69 @@
+from typing import List
+import collections
+import itertools
+import functools
+import math
+import string
+import random
+import bisect
+import re
+import operator
+import heapq
+import queue
+
+from queue import PriorityQueue
+from itertools import combinations, permutations
+from functools import lru_cache
+from collections import defaultdict
+from collections import OrderedDict
+from collections import deque
+from collections import Counter
+
+'''
+https://neetcode.io/problems/longest-consecutive-sequence
+'''
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for e in nums:
+            if (e - 1) in numSet:
+                # skip since we're not the potential start of a sequence
+                continue
+            
+            # check rest of sequence
+            l = 1
+            while (e + l) in numSet:
+                l = l + 1
+            
+            longest = max(l, longest)
+
+        return longest
+
+
+
+if __name__ == '__main__':
+    # stupid...but works
+    import sys, os
+
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+    from TestHarness import *
+
+    s = Solution()
+    sf = s.longestConsecutive
+
+    check_solution_simple(
+        sf,
+        args=[[2,20,4,10,3,4,5]],
+        expected=4
+    )
+
+    check_solution_simple(
+        sf,
+        args=[[0,3,2,5,4,6,1,1]],
+        expected=7
+    )
