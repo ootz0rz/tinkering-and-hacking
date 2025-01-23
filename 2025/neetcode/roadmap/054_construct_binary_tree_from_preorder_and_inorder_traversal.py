db3157167e1960a98f9138b77e8455f615276f9b
H S <ootz0rz@gmail.com>
2025-01-22 22:18:58 -0500

    noice, first go

---
+++ 2025/neetcode/roadmap/054_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -0,0 +1,78 @@
+from typing import List,Optional
+import collections
+import itertools
+import functools
+import math
+import string
+import random
+import bisect
+import re
+import operator
+import heapq
+import queue
+
+from queue import PriorityQueue
+from itertools import combinations, permutations
+from functools import lru_cache
+from collections import defaultdict
+from collections import OrderedDict
+from collections import deque
+from collections import Counter
+
+# ------------ BinaryTree
+# stupid...but works
+import sys, os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from BinaryTree import *
+# ------------ BinaryTree
+
+# https://neetcode.io/problems/kth-smallest-integer-in-bst
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+        def build(p, i):
+            if len(p) == 0:
+                return None
+            
+            rv = p[0]
+            root = TreeNode(rv)
+            
+            # find the split of inorder
+            for idx, e in enumerate(i):
+                if e == rv:
+                    break
+
+            root.left = build(p[1:1+idx], i[:idx])
+
+            if len(p) > idx:
+                root.right = build(p[idx+1:], i[idx+1:])
+
+            return root
+        
+        return build(preorder, inorder)
+
+
+
+if __name__ == '__main__':
+    # stupid...but works
+    import sys, os
+
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+    from TestHarness import *
+
+    s = Solution()
+    sf = s.diameterOfBinaryTree
+
