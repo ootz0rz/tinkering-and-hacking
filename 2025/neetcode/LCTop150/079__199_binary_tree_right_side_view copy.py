a950688c81bd13032a473731738cb19a029dab08
H S <ootz0rz@gmail.com>
2025-03-04 23:19:37 -0500

    stuff.

---
+++ 2025/neetcode/LCTop150/079__199_binary_tree_right_side_view copy.py
@@ -0,0 +1,76 @@
+from typing import List,Optional
+import collections
+import itertools
+import functools
+import math
+import string
+import random
+import bisect
+import re
+import operator
+import heapq
+import queue
+
+from queue import PriorityQueue
+from itertools import combinations, permutations
+from functools import lru_cache
+from collections import defaultdict
+from collections import OrderedDict
+from collections import deque
+from collections import Counter
+
+# ------------ BinaryTree
+# stupid...but works
+import sys, os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from BinaryTree import *
+# ------------ BinaryTree
+
+# https://leetcode.com/problems/binary-tree-right-side-view/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        
+        q = deque([root])
+
+        o = [] 
+        while len(q) > 0:
+
+            # e = None
+            for _ in range(len(q)):
+                e = q.popleft()
+
+                if e.left:
+                    q.append(e.left)
+
+                if e.right:
+                    q.append(e.right)
+            
+            o.append(e.val)
+        
+        return o
+        
+
+if __name__ == '__main__':
+    # stupid...but works
+    import sys, os
+
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+    from TestHarness import *
+
+    s = Solution()
+    sf = s.rightSideView
+
+    t = [1,2,3,None,5,None,4]
+    tr = GEN_TREE_FROM_LIST(t)
+    print(f"Tree: {tr.__all__()}")
+
+    print(sf(tr))
+
